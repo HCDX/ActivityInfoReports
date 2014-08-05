5ce543b87f39a159eaf8b1491d50d2c6fafc44bc
jamescw <jcranwellward@gmail.com>
2014-08-05 19:45:05 +0300

    First version

---
+++ aggregator.py
@@ -0,0 +1,90 @@
+
+from flask import Flask
+
+from flask.ext import admin
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.admin.contrib.mongoengine import ModelView
+
+# Create application
+app = Flask(__name__)
+
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+app.config['MONGODB_SETTINGS'] = {'DB': 'ai'}
+
+# Create models
+db = MongoEngine()
+db.init_app(app)
+
+
+# Define mongoengine documents
+class Attribute(db.EmbeddedDocument):
+    name = db.StringField()
+    value = db.StringField()
+
+
+class Report(db.Document):
+    date = db.StringField()
+    category = db.StringField()
+    activity_id = db.IntField()
+    activity = db.StringField()
+    partner_id = db.IntField()
+    partner_name = db.StringField()
+    location_id = db.IntField()
+    location_name = db.StringField()
+    location_x = db.DecimalField()
+    location_y = db.DecimalField()
+    indicator_id = db.IntField()
+    indicator_name = db.StringField()
+    value = db.DecimalField()
+    attributes = db.ListField(
+        db.EmbeddedDocumentField(Attribute)
+    )
+
+
+# Customized admin views
+class ReportView(ModelView):
+    can_create = False
+    can_delete = False
+    can_edit = False
+
+    column_filters = [
+        'date',
+        'category',
+        'activity',
+        'partner_name',
+        'location_name',
+        'indicator_name',
+    ]
+
+    column_searchable_list = column_filters
+
+    form_subdocuments = {
+        'attributes': {
+            'form_subdocuments': {
+                None: {
+                    'form_columns': ('name', 'value',)
+                }
+            }
+
+        }
+    }
+
+
+# Create admin
+admin = admin.Admin(app, 'ActivityInfo Reports')
+
+# Add views
+admin.add_view(ReportView(Report))
+
+
+# Flask views
+@app.route('/')
+def index():
+    return '<a href="/admin/">Click me to get to Admin!</a>'
+
+
+if __name__ == '__main__':
+
+    # Start app
+    app.run(debug=True)
---
+++ manage.py
@@ -0,0 +1,101 @@
+__author__ = 'jcranwellward'
+
+import datetime
+
+from flask.ext.script import (
+    Command,
+    Manager,
+    Option,
+    Server
+)
+
+from pymongo import MongoClient
+from activtyinfo_client import ActivityInfoClient
+
+from aggregator import app, Report, Attribute
+
+manager = Manager(app)
+
+@manager.command
+def import_ai(db, username='', password=''):
+    """
+    Imports data from Activity Info
+    """
+    db_id = db
+    client = ActivityInfoClient(username, password)
+    db_info = client.get_database(db_id)
+
+    mongo = MongoClient()
+    ai = mongo['ai']
+    # store the whole database for future reference
+    print 'Pulling database...'
+    ai.databases.update({'_id': db_id}, db_info, upsert=True)
+    # split out all the attribute groups into a separate collection
+    attribs = ai.databases.aggregate([
+        {'$project': {'groups': '$activities.attributeGroups'}},
+        {'$unwind': '$groups'},
+        {'$unwind': '$groups'},
+        {'$group': {'_id': "$_id", 'groups': {'$push': '$groups'}}},
+    ])
+    for attrib in attribs['result'][0]['groups']:
+        ai.attributeGroups.update({'_id': attrib['id']}, attrib, upsert=True)
+
+    for activity in ai.databases.find_one({'_id': db_id})['activities']:
+        print 'Pulling sites for activity: {} - {}'.format(activity['id'], activity['name'])
+        sites = client.get_sites(activity=activity['id'], include_monthly_reports=False)
+        for site in sites:
+            attributes = [attr for attr in ai.attributeGroups.find(
+                {'attributes.id': {'$in': site['attributes']}},
+                {'name': 1, 'mandatory': 1, "attributes.$": 1}
+            )]
+
+            print 'Pulling reports for site: {} - {}'.format(
+                site['id'],
+                site['location']['name'].encode('UTF-8')
+            )
+            reports = client.get_monthly_reports_for_site(site['id'])
+            for date, indicators in reports.items():
+                for indicator in indicators:
+                    report, created = Report.objects.get_or_create(
+                        date=date,
+                        category=activity['category'],
+                        activity_id=activity['id'],
+                        activity=activity['name'],
+                        partner_id=site['partner']['id'],
+                        partner_name=site['partner']['name'],
+                        location_id=site['location']['id'],
+                        location_name=site['location']['name'],
+                        location_x=site['location'].get('longitude', None),
+                        location_y=site['location'].get('latitude', None),
+                        indicator_id=indicator['indicatorId'],
+                        indicator_name=indicator['indicatorName'],
+                        value=indicator['value']
+                    )
+                    if created:
+                        for a in attributes:
+                            report.attributes.append(
+                                Attribute(
+                                    name=a['name'],
+                                    value=a['attributes'][0]['name']
+                                )
+                            )
+                        report.save()
+                        print 'Created report: {} -> {} -> {} -> {} = {}'.format(
+                            report.date,
+                            report.location_name,
+                            report.partner_name,
+                            report.indicator_name,
+                            report.value
+                        )
+
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger=True,
+    use_reloader=True,
+    host='0.0.0.0')
+)
+
+
+if __name__ == "__main__":
+    manager.run()
