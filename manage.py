50ae996280eecf47f65b0ace2dbfe456e4e4d567
jamescw <jcranwellward@gmail.com>
2014-08-06 13:24:43 +0300

    try again

--- manage.py
+++ manage.py
@@ -90,7 +90,7 @@ def import_ai(ai_db, username='', password=''):
                                 report.value
                             )
     except Exception as exp:
-        print str(exp).encode()
+        print str(exp).encode('utf-8')
 
 
 # Turn on debugger by default and reloader
